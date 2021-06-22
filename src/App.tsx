8fe533d59021264d27d4fc0a6de3584c98429ad4
Gabriel Araujo <gabriiel66@gmail.com>
2021-06-22 19:31:33 -0300

    context & hooks

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import { BrowserRouter, Route } from 'react-router-dom'
 import { Home } from 'pages/Home'
 import { NewRoom } from 'pages/NewRoom'
 
-import 'styles/global.scss'
+import { AuthContextProvider } from 'contexts/AuthContext'
 
 function App() {
   return (
     <BrowserRouter>
-      <Route path="/" exact component={Home} />
-      <Route path="/rooms/new" component={NewRoom} />
+      <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>
     </BrowserRouter>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,22 @@
 import { useHistory } from 'react-router-dom'
-import { auth, firebase } from 'services/firebase'
+
+import { useAuth } from 'hooks/useAuth'
+import { Button } from 'components/Button'
 
 import illustrationImg from 'assets/images/illustration.svg'
 import logoImg from 'assets/images/logo.svg'
 import googleIconImg from 'assets/images/google-icon.svg'
 
-import { Button } from 'components/Button'
-
 import 'styles/auth.scss'
 
 export function Home() {
+  const { user, signInWithGoogle } = useAuth()
   const history = useHistory()
 
-  function handleCreateRoom() {
-    const provider = new firebase.auth.GoogleAuthProvider()
-
-    auth.signInWithPopup(provider).then((result) => {
-      console.log(result)
-    })
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle()
+    }
 
     history.push('/rooms/new')
   }
