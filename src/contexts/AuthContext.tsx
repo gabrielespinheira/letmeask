80366d9a31b360ffa7d14500f378df186aaabb0c
Gabriel Araujo <gabriiel66@gmail.com>
2021-06-23 20:28:22 -0300

    fix

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -43,7 +43,7 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
     return () => {
       unsubscribe()
     }
-  }, [user])
+  }, [])
 
   async function signInWithGoogle() {
     const provider = new firebase.auth.GoogleAuthProvider()
